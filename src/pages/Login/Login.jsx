8c2499973adead54461c2e703091503a626cffa4
Islomjonsdev <islomjon@immigrateable.com>
2025-02-08 14:06:56 +0500

    fiexxd

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
