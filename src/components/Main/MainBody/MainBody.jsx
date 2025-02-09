045b5335735862c44ed1f8767fda0811a88249dc
Islomjonsdev <islomjon@immigrateable.com>
2025-02-09 23:04:49 +0500

    fixedewe

--- src/components/Main/MainBody/MainBody.jsx
+++ src/components/Main/MainBody/MainBody.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { instance } from "@/api";
+import { useState, useEffect } from "react";
 
 const MainBody = () => {
-  return (
-    <div className='px-[15px]'>MainBody</div>
-  )
-}
+  const [user, setUser] = useState([]);
+  const token = localStorage.getItem("usertoken");
+  console.log(user);
 
-export default MainBody
+  useEffect(() => {
+    instance
+      .get("/auths/get-info", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return <div className="px-[15px]">MainBody</div>;
+};
+
+export default MainBody;
